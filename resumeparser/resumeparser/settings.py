7941ecf13ffe8cecc9d6e8c1abb7b37183e2f814
anubhavlal07 <anubhavlal.15@gmail.com>
2025-06-01 16:20:00 +0530

    Update database configuration in settings.py: switch from SQLite to PostgreSQL and load environment variables

--- resumeparser/resumeparser/settings.py
+++ resumeparser/resumeparser/settings.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -47,8 +50,12 @@ WSGI_APPLICATION = 'resumeparser.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
 
